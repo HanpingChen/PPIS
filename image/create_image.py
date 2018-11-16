a8998be938f5897491e57aebccbb94c34e1b91e6
LionsChen <chenlions@163.com>
2018-11-16 19:42:12 +0800

    批量写图片

--- image/create_image.py
+++ image/create_image.py
@@ -7,13 +7,23 @@ from PIL import ImageDraw
 from PIL import ImageFont
 from prepare_data import generate_poly_seq
 p = PDBParser()
+hy_dict = {
+    'R': -4.5, 'K': -3.9, 'N': -3.5, 'D': -3.5, 'Q': -3.5, 'E': -3.5, 'H': -3.2, 'P': -1.6,
+    'Y': -1.3, 'W': -0.9, 'S': -0.8, 'T': -0.7, 'G': -0.4, 'A': 1.8, 'M': 1.9, 'C': 2.5,
+    'F': 2.8, 'L': 3.8, 'V': 4.2, 'I': 4.5
+}
+
+
+def get_f(aa):
+    return int(-28*hy_dict[aa]+126)
+
 
 structure_id = "1aby"
-clean_file_name = "../clean_data/"+structure_id+".ent"
+clean_file_name = "E:\PPIS\pdb\pdb"+structure_id+".ent"
 structure = p.get_structure(structure_id, clean_file_name)
 dict = generate_poly_seq(structure)
 print(dict)
-img = Image.new("RGB", (512, 512), (0, 0, 0))#RGB模式
+img = Image.new("P", (256, 256), 0)#RGB模式
 draw = ImageDraw.Draw(img)
 import json
 with open("../json/1aby.json","r") as f:
@@ -45,37 +55,43 @@ print(count_dict)
 for keys in count_dict.keys():
     if keys not in site_count_dict.keys():
         site_count_dict[keys] = 0
-x = -10
-y = 5
+x = -5
+y = 0
 for i in range(len(chain_a)):
-    x = x + 10
-    if x > 500:
+    x = x + 5
+    if x > 249:
         x = 0
-        y = y + 10
-    draw.text((x, y), chain_a[i], fill=(count_dict[chain_a[i]]+100,site_count_dict[chain_a[i]]*10+50, count_dict[chain_a[i]]+50))
+        y = y + 5
+    for k in range(x, x + 5):
+        for l in range(y, y + 5):
+
+            img.putpixel((k, l), int(get_f(chain_a[i])))
 # for i in range(2000):
 #     for j in range(500):
 #         r, g, b = img.getpixel((i, j))
 #         if r != 0 and g != 0 and b != 0:
 #             img.putpixel((i, j), (120, 230, 130))
 img.show()
-
-label = Image.new("P", (512, 512), 0)
+base_img_path = "E:\\PPIS\\train_data\\image\\"
+label = Image.new("P", (256, 256), 0)
 draw = ImageDraw.Draw(label)
 
+img.save(base_img_path+structure_id+".png", "png")
 x = -5
 y = 0
 for i in range(len(chain_a)):
-    x = x +5
-    if x > 500:
+    x = x + 5
+    if x > 249:
         x = 0
-        y = y + 10
+        y = y + 5
     for k in range(x, x + 5):
         for l in range(y, y + 5):
             if i in site_index_set:
                 label.putpixel((k, l), 255)
+                print(k, l)
             else:
                 label.putpixel((k, l), 0)
 
-
+base_label_path = "E:\\PPIS\\train_data\\label\\"
+label.save(base_label_path+structure_id+".png", "png")
 label.show()
--- image/create_train_image.py
+++ image/create_train_image.py
@@ -11,6 +11,17 @@ base_label_path = "E:\\PPIS\\train_data\\label\\"
 base_pdb_path = "E:\\PPIS\\pdb\\"
 base_json_path = "E:\\PPIS\\train_data\\json\\"
 
+hy_dict = {
+    'R': -4.5, 'K': -3.9, 'N': -3.5, 'D': -3.5, 'Q': -3.5, 'E': -3.5, 'H': -3.2, 'P': -1.6,
+    'Y': -1.3, 'W': -0.9, 'S': -0.8, 'T': -0.7, 'G': -0.4, 'A': 1.8, 'M': 1.9, 'C': 2.5,
+    'F': 2.8, 'L': 3.8, 'V': 4.2, 'I': 4.5
+}
+
+
+def get_pix_value(aa):
+    return int(-28*hy_dict[aa]+126)
+
+import math
 count = 0
 file_list = os.listdir(base_json_path)
 for file in file_list:
@@ -27,7 +38,13 @@ for file in file_list:
         protein_path = os.path.join(base_pdb_path, "pdb"+structure_id+".ent")
         structure = p.get_structure(structure_id, protein_path)
         dict = generate_poly_seq(structure)
-        img = Image.new("P", (512, 512), 0)  # 普通八位模式
+        length = 0
+        for item in dict.items():
+            length += len(dict[item[0]])
+            break
+        width = (int(math.sqrt(length * 25)) + 1)
+        height = (int(math.sqrt(length * 25)) + 1)
+        img = Image.new("P", (width, height), 0)  # 普通八位模式
         draw = ImageDraw.Draw(img)
         for item in dict.items():
             chain_id = item[0]
@@ -37,33 +54,43 @@ for file in file_list:
             load_dict = json.load(f)
         chain_site = load_dict[chain_id]
         # 绘制序列
-        x = -10
-        y = 5
+        x = -5
+        y = 0
         for i in range(len(chain_seq)):
-            x = x + 10
-            if x > 500:
+            x = x + 5
+            if x > width -6:
                 x = 0
-                y = y + 10
-            draw.text((x, y), chain_seq[i], fill=255)
+                y = y + 5
+                if y > height - 6:
+                    break
+            # img.putpixel((x, y), get_pix_value(chain_seq[i % (len(chain_seq))]))
+            for k in range(x, x + 5):
+                for l in range(y, y + 5):
+                    img.putpixel((k, l), get_pix_value(chain_seq[i]))
+        img = img.resize((256, 256))
         img.save(base_img_path + structure_id + ".png", "png")
         #img.show()
         # 绘制label
-        label = Image.new("P", (512, 512), 0)
+        label = Image.new("P", (width, height), 0)
         draw = ImageDraw.Draw(label)
         site_index_set = set()
         for item in chain_site:
             site_index_set.add(item['seq_index'])
-        x = -10
-        y = 5
+        x = -5
+        y = 0
         for i in range(len(chain_seq)):
-            x = x + 10
-            if x > 500:
+            x = x + 5
+            if x > width-6:
                 x = 0
-                y = y + 10
+                y = y + 5
+                if y > height-6:
+                    break
+
             for k in range(x, x + 5):
                 for l in range(y, y + 5):
                     if i in site_index_set:
                         label.putpixel((k, l), 255)
                     else:
                         label.putpixel((k, l), 0)
+        label = label.resize((256, 256))
         label.save(base_label_path + structure_id + ".png", "png")
