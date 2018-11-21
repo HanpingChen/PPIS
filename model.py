e11109a04c9afd2e9213f5962557f2e9109654d5
LionsChen <chenlions@163.com>
2018-11-21 13:24:20 +0800

    增加recall，precision，f1的计算

--- model.py
+++ model.py
@@ -54,22 +54,59 @@ class Metrics(Callback):
 metrics = Metrics()
 
 
+def precision(y_true, y_pred):
+    class_id_true = K.argmax(y_true, axis=-1)
+    class_id_preds = K.argmax(y_pred, axis=-1)
+    accuracy_mask = K.cast(K.equal(class_id_preds, 1), 'int32')
+    class_acc_tensor = K.cast(K.equal(class_id_true, class_id_preds), 'int32') * accuracy_mask
+    class_acc = K.sum(class_acc_tensor) / K.maximum(K.sum(accuracy_mask), 1)
+    return class_acc
+
+
+def recall(y_true, y_pred):
+
+    # 预测
+    class_pred = K.argmax(y_pred, axis=-1)
+    # 所有的正样本
+    class_true = K.argmax(y_true, axis=-1)
+    # 预测为正样本的
+    accuracy_mask = K.cast(K.equal(class_pred, 1), 'int32')
+    # 预测正确的正样本
+    class_acc_tensor = K.cast(K.equal(class_true, class_pred), 'int32') * accuracy_mask
+    # 所有正样本
+    P = K.cast(K.equal(class_true, 1), 'int32')
+    recall = K.sum(class_acc_tensor) / K.maximum(K.sum(P), 1)
+    return recall
+
+
+def f1(y_true, y_pred):
+    pre = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    pre = K.cast(pre, 'float32')
+    r = K.cast(r, 'float32')
+    return 2*(pre * r) / (pre + r)
+
+
 tbCallBack = keras.callbacks.TensorBoard(log_dir='log',
 histogram_freq=1,
 write_graph=True,
 write_images=True)
 # #
 from keras.optimizers import Adam
-model.load_weights("model_v1.hdf5")
-sgd = optimizers.SGD(lr=0.001, decay=0.09, momentum=0.8, nesterov=True)
+# model.load_weights("model_v1.hdf5")
+sgd = optimizers.SGD(lr=0.01, decay=0.09, momentum=0.8, nesterov=True)
 model_checkpoint = ModelCheckpoint('model_v1.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy', precision, recall, f1])
 # X, Y = load_data(10000, "tfrecord/train.tfrecord")
 # X_val, Y_val = load_data(2000, "tfrecord/test.tfrecord")
+num_non = 30699
+num_site = 5505
+# t 在0到1之间，t越大，则约优化recall，t越小，越优化precision
+t = 0.48
 model.fit_generator(train_generator,
                     callbacks=[model_checkpoint,metrics],
                     epochs=20,
-                    class_weight={1: 3.62, 0: 1},
+                    class_weight={1: (num_non / num_site)*t, 0: 1},
                     validation_data=test_generator,
                     shuffle=True,
 
