d6a94f8f174a1aa569e92eb93d97c714fe41712a
LionsChen <chenlions@163.com>
2018-11-21 14:16:10 +0800

    模型

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ train_datagen = ImageDataGenerator(
     rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(  # 以文件夹路径为参数,自动生成经过数据提升/归一化后的数据和标签
-    '/Users/chenhanping/data/ppis/train/',  # 训练数据路径，train 文件夹下包含每一类的子文件夹
+    'data/ppis/train/',  # 训练数据路径，train 文件夹下包含每一类的子文件夹
     target_size=(150, 150),  # 图片大小resize成 150x150
     batch_size=32,
     class_mode='categorical')  # 使用二分类，返回2-D 的二值标签
@@ -23,7 +23,7 @@ test_datagen = ImageDataGenerator(
     rescale=1. / 255)
 
 test_generator = test_datagen.flow_from_directory(  # 以文件夹路径为参数,自动生成经过数据提升/归一化后的数据和标签
-    '/Users/chenhanping/data/ppis/test/',  # 训练数据路径，train 文件夹下包含每一类的子文件夹
+    'data/ppis/test/',  # 训练数据路径，train 文件夹下包含每一类的子文件夹
     target_size=(150, 150),  # 图片大小resize成 150x150
     batch_size=32,
     class_mode='categorical')  # 使用二分类，返回2-D 的二值标签
@@ -34,18 +34,21 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 
 
 class Metrics(Callback):
+    import numpy as np
     def on_train_begin(self, logs={}):
         self.val_f1s = []
         self.val_recalls = []
         self.val_precisions = []
 
     def on_epoch_end(self, epoch, logs={}):
-        val_predict = (numpy.asarray(self.model.predict(
-            self.validation_data[0]))).round()
-        val_targ = self.validation_data[1]
-        _val_f1 = f1_score(val_targ, val_predict,average='macro')
-        _val_recall = recall_score(val_targ, val_predict,average='macro')
-        _val_precision = precision_score(val_targ, val_predict,average='macro')
+        batch_result = self.model.predict(self.validation_data[0])
+        val_predict = numpy.argmax(batch_result, axis=1)
+        val_targ = self.validation_data[1][:,1]
+        print("pred", val_predict)
+        print("y_true", val_targ)
+        _val_f1 = f1_score(val_targ, val_predict)
+        _val_recall = recall_score(val_targ, val_predict)
+        _val_precision = precision_score(val_targ, val_predict)
         self.val_f1s.append(_val_f1)
         self.val_recalls.append(_val_recall)
         self.val_precisions.append(_val_precision)
@@ -60,16 +63,16 @@ write_graph=True,
 write_images=True)
 # #
 from keras.optimizers import Adam
-model.load_weights("model_v1.hdf5")
+# model.load_weights("model_v1.hdf5")
 sgd = optimizers.SGD(lr=0.001, decay=0.09, momentum=0.8, nesterov=True)
-model_checkpoint = ModelCheckpoint('model_v1.hdf5', monitor='loss',verbose=1, save_best_only=True)
+model_checkpoint = ModelCheckpoint('model_v1.hdf5', monitor='loss', verbose=1, save_best_only=True)
 model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 # X, Y = load_data(10000, "tfrecord/train.tfrecord")
 # X_val, Y_val = load_data(2000, "tfrecord/test.tfrecord")
 model.fit_generator(train_generator,
-                    callbacks=[model_checkpoint,metrics],
-                    epochs=20,
-                    class_weight={1: 3.62, 0: 1},
+                    callbacks=[model_checkpoint, metrics],
+                    epochs=10,
+                    class_weight={1: 3, 0: 1},
                     validation_data=test_generator,
                     shuffle=True,
 
@@ -92,22 +95,7 @@ model.fit_generator(train_generator,
 #     batch_size=32,
 #     class_mode='categorical')  # 使用二分类，返回1-D 的二值标签
 
-from keras.preprocessing import image
-from keras.applications.inception_v3 import preprocess_input
-from keras.applications import imagenet_utils
-import os
-file_list = os.listdir('data/test/site/')
-for file in file_list:
-    file_path = os.path.join('data/test/site/', file)
-    img = image.load_img(file_path)
-    x = image.img_to_array(img)
-    import numpy as np
-    x = np.expand_dims(x, axis=0)
-    # x = preprocess_input(x)
-    x /= .255
-    result = model.predict(x)
 
-    print(result)
 # result = model.predict_generator(test_generator)
 # score = model.evaluate_generator(train_generator,steps=128)
 # print(score)
